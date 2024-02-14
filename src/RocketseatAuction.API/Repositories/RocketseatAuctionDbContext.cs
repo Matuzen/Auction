041058ea445d9233524a74550d37315e81aa10c9
Matheus Alexandre <matheuscurses@gmail.com>
2024-02-14 17:47:54 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+    public DbSet<Auction> Auctions { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\dev\\RocketseatAuction\\leilaoDbNLW.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
