6014c35e24c3898df70fb3dc121fc36aea940d53
Matheus Alexandre <matheuscurses@gmail.com>
2024-02-15 15:36:38 -0300

    Recebendo o ItemId

---
+++ src/RocketseatAuction.API/Communication/Requests/RequestCreateOfferJson.cs
@@ -0,0 +1,9 @@
+﻿namespace RocketseatAuction.API.Communication.Requests;
+
+public class RequestCreateOfferJson
+{
+    public decimal Price { get; set; }
+}
+
+
+// Para criar uma oferta precisamos do usuário e do preço 
--- src/RocketseatAuction.API/Controllers/OfferController.cs
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Communication.Requests;
 
 namespace RocketseatAuction.API.Controllers;
 
 public class OfferController : RocketseatAuctionBaseController
 {
     [HttpPost]
-    public IActionResult CreateOffer()
+    [Route("{itemId}")]
+    public IActionResult CreateOffer([FromRoute] int itemId, [FromBody] RequestCreateOfferJson request)
     {
         return Created();
     }
 }
+
+
+// Uma oferta está associada a um item
+// Como eu preciso passar o Id de um Iem? Como eu vejo o Id de um Produto? - Quando recuperar os leilões que contém os Ids dos Produtos
+// Precisa enviar o endpoint o Id do produto que desejar criar uma oferta para ele, passando o Id pela URL(Route)
+// Este endpoint precisa receber o objeto RequestCreateOfferJson
