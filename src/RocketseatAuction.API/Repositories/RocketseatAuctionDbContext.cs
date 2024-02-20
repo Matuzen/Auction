6ffd3f8d872bc148936bcd3de99482c65e4fc317
Matheus Alexandre <matheuscurses@gmail.com>
2024-02-20 09:33:56 -0300

    Finalizado meu projeto de leilões

--- src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -5,15 +5,8 @@ namespace RocketseatAuction.API.Repositories;
 
 public class RocketseatAuctionDbContext : DbContext
 {
+    public RocketseatAuctionDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Auction> Auctions { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("Data Source=C:\\dev\\RocketseatAuction\\leilaoDbNLW.db");
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-    }
-
+    public DbSet<User> Users { get; set; }
+    public DbSet<Offer> Offers { get; set; }
 }
